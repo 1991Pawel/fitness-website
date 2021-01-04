3db74d16e6f6199332a17bf724ca13d32916a39a
Pawel L <virtuozoo@o2.pl>
2021-01-04 01:12:59 +0100

    add frame-motion animation

--- components/IntroSection/IntroSection.jsx
+++ components/IntroSection/IntroSection.jsx
@@ -1,24 +1,81 @@
 import React from 'react';
 import styles from 'styles/IntroSection.module.scss';
+import { motion } from 'framer-motion';
 
-const IntroSection = () => (
-  <section id="intro" className={styles.intro}>
-    <div className={styles.intro__inner}>
-      <h1 className={styles.title}>
-        Trener<span className={styles.title__special}>Monika Korzeniecka</span>
-      </h1>
-      <p className={styles.desc}>
-        Wyznacz swoje cele, a ja pomogę Ci je zrealizować.
-      </p>
-      <a href="#about" className={styles.btn}>
-        Poznaj mnie
-      </a>
-      <div className={styles.footer}>
-        <span className={styles.footer__text}>Trening</span>
-        <span className={styles.footer__text}>Dieta</span>
-        <span className={styles.footer__text}>Motywacja</span>
-      </div>
+const IntroSection = () => {
+  const container = {
+    hidden: { y: '100%', opacity: 1 },
+    show: {
+      y: '-100%',
+      transition: {
+        delay: 0.3,
+        duration: 1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: '100%' },
+    show: {
+      y: '0%',
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+      },
+    },
+  };
+
+  const content = {
+    hidden: { opacity: 0, y: '20%' },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.2,
+        duration: 0.8,
+      },
+    },
+  };
+
+  return (
+    <div className={styles.section__wrapper}>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className={styles.overlay}
+      />
+      <motion.section
+        initial="hidden"
+        animate="show"
+        variants={item}
+        id="intro"
+        className={styles.intro}
+      >
+        <motion.div
+          initial="hidden"
+          animate="show"
+          variants={content}
+          className={styles.intro__inner}
+        >
+          <h1 className={styles.title}>
+            Trener
+            <span className={styles.title__special}>Monika Korzeniecka</span>
+          </h1>
+          <p className={styles.desc}>
+            Wyznacz swoje cele, a ja pomogę Ci je zrealizować.
+          </p>
+          <a href="#about" className={styles.btn}>
+            Poznaj mnie
+          </a>
+          <div className={styles.footer}>
+            <span className={styles.footer__text}>Trening</span>
+            <span className={styles.footer__text}>Dieta</span>
+            <span className={styles.footer__text}>Motywacja</span>
+          </div>
+        </motion.div>
+      </motion.section>
     </div>
-  </section>
-);
+  );
+};
 export default IntroSection;
