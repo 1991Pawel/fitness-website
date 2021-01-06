8988ff50db7be933edb6fdb9ea0b779bcc53064c
Pawel L <virtuozoo@o2.pl>
2021-01-06 02:51:09 +0100

    add dynamic cononical

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -4,26 +4,31 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import Head from 'next/head';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
+import { useRouter } from 'next/router';
 
 const queryClient = new QueryClient();
 
-const Layout = ({ children }) => (
-  <QueryClientProvider client={queryClient}>
-    <Head>
-      <title>Monika Korzeniecka | Trener</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="canonical" href="https://korzeniecka.pl/" />
-      <meta
-        name="Description"
-        content="Monika Korzeniecka, Konsultacje dietetyczne i treningowe, prowadzenie online, treningi personalne"
-      />
-    </Head>
-    <Header />
-    <main>{children}</main>
-    <Footer />
-  </QueryClientProvider>
-);
+const Layout = ({ children }) => {
+  const site = 'https://korzeniecka.pl';
+  const canURL = site + useRouter().pathname;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Monika Korzeniecka | Trener</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="canonical" href={canURL} />
+        <meta
+          name="Description"
+          content="Monika Korzeniecka, Konsultacje dietetyczne i treningowe, prowadzenie online, treningi personalne"
+        />
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </QueryClientProvider>
+  );
+};
 export default Layout;
 
 Layout.propTypes = { children: PropTypes.node.isRequired };
